66a9a45789dfb61b23d2b1edb104a1a8c2114089
Mykle Nero <mykle.nero@gmail.com>
2024-03-31 20:26:19 -0400

    create initial websockets canister

---
+++ src/declarations/websockets/websockets.did.d.ts
@@ -0,0 +1,60 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface AppMessage { 'message' : string }
+export interface CanisterOutputCertifiedMessages {
+  'messages' : Array<CanisterOutputMessage>,
+  'cert' : Uint8Array | number[],
+  'tree' : Uint8Array | number[],
+  'is_end_of_queue' : boolean,
+}
+export interface CanisterOutputMessage {
+  'key' : string,
+  'content' : Uint8Array | number[],
+  'client_key' : ClientKey,
+}
+export interface CanisterWsCloseArguments { 'client_key' : ClientKey }
+export type CanisterWsCloseResult = { 'Ok' : null } |
+  { 'Err' : string };
+export interface CanisterWsGetMessagesArguments { 'nonce' : bigint }
+export type CanisterWsGetMessagesResult = {
+    'Ok' : CanisterOutputCertifiedMessages
+  } |
+  { 'Err' : string };
+export interface CanisterWsMessageArguments { 'msg' : WebsocketMessage }
+export type CanisterWsMessageResult = { 'Ok' : null } |
+  { 'Err' : string };
+export interface CanisterWsOpenArguments {
+  'gateway_principal' : GatewayPrincipal,
+  'client_nonce' : bigint,
+}
+export type CanisterWsOpenResult = { 'Ok' : null } |
+  { 'Err' : string };
+export interface ClientKey {
+  'client_principal' : ClientPrincipal,
+  'client_nonce' : bigint,
+}
+export type ClientPrincipal = Principal;
+export type GatewayPrincipal = Principal;
+export interface WebsocketMessage {
+  'sequence_num' : bigint,
+  'content' : Uint8Array | number[],
+  'client_key' : ClientKey,
+  'timestamp' : bigint,
+  'is_service_message' : boolean,
+}
+export interface _SERVICE {
+  'ws_close' : ActorMethod<[CanisterWsCloseArguments], CanisterWsCloseResult>,
+  'ws_get_messages' : ActorMethod<
+    [CanisterWsGetMessagesArguments],
+    CanisterWsGetMessagesResult
+  >,
+  'ws_message' : ActorMethod<
+    [CanisterWsMessageArguments, [] | [AppMessage]],
+    CanisterWsMessageResult
+  >,
+  'ws_open' : ActorMethod<[CanisterWsOpenArguments], CanisterWsOpenResult>,
+}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
